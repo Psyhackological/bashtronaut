0a68be499810ba3ba610b8757fc2be9bc44f707f
Psyhackological <konradkon@duck.com>
2023-07-03 21:25:55 +0200

    Added shellcheck suggestions to mkv_to_txt.sh

--- scripts/mkv_to_txt.sh
+++ scripts/mkv_to_txt.sh
@@ -5,21 +5,23 @@ function extract_subtitle {
 	local LANGUAGE_CODE=$2
 
 	# Construct the output file names
-	local BASE_NAME=$(basename "${VIDEO_FILE}" .mkv)
+	local BASE_NAME
+	BASE_NAME=$(basename "$VIDEO_FILE" .mkv)
 	local SUBTITLE_FILE="${BASE_NAME}_${LANGUAGE_CODE}.srt"
 	local CLEAN_SUBTITLE_FILE="${BASE_NAME}_${LANGUAGE_CODE}.txt"
 
 	# Find the index of the subtitle stream in the selected language
-	local SUBTITLE_INDEX=$(ffprobe -v error -select_streams s -show_entries stream=index:stream_tags=language -of csv=p=0 "${VIDEO_FILE}" | awk -F, -v lang="${LANGUAGE_CODE}" '$2==lang {print $1-2}')
+	local SUBTITLE_INDEX
+	SUBTITLE_INDEX=$(ffprobe -v error -select_streams s -show_entries stream=index:stream_tags=language -of csv=p=0 "$VIDEO_FILE" | awk -F, -v lang="$LANGUAGE_CODE" '$2==lang {print $1-2}')
 
 	# Check if subtitle file exists
-	if [[ ! -f "${SUBTITLE_FILE}" ]]; then
+	if [[ ! -f "$SUBTITLE_FILE" ]]; then
 		# Extract subtitles if the file does not exist
-		ffmpeg -i "${VIDEO_FILE}" -map 0:s:${SUBTITLE_INDEX} -c:s srt -y "${SUBTITLE_FILE}"
+		ffmpeg -i "$VIDEO_FILE" -map 0:s:"$SUBTITLE_INDEX" -c:s srt -y "$SUBTITLE_FILE"
 	fi
 
 	# Remove timestamps, digits, and empty lines and redirect to clean subtitle file
-	sed -r '/^[0-9]+$/d;/^[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}$/d;/^$/d' "${SUBTITLE_FILE}" >"${CLEAN_SUBTITLE_FILE}"
+	sed -r '/^[0-9]+$/d;/^[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}$/d;/^$/d' "$SUBTITLE_FILE" >"$CLEAN_SUBTITLE_FILE"
 }
 
 function main {
@@ -32,7 +34,7 @@ function main {
 
 	# Iterate over all .mkv files in the current directory
 	for VIDEO_FILE in *.mkv; do
-		extract_subtitle "${VIDEO_FILE}" "${LANGUAGE_CODE}"
+		extract_subtitle "$VIDEO_FILE" "$LANGUAGE_CODE"
 	done
 }
 
