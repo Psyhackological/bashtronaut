90a85ce0bc6a5e43037452c238abe269a59a88fe
Psyhackological <konradkon@duck.com>
2024-01-11 21:44:06 +0100

    Add linter suggestion (tr '[:lower:]' '[:upper:]')

--- scripts/md_to_upper.sh
+++ scripts/md_to_upper.sh
@@ -3,5 +3,5 @@
 for file in *.md; do
 	filename=$(basename -- "$file")
 	name_only="${filename%.*}"
-	git mv "$file" "$(echo "$name_only" | tr 'a-z' 'A-Z').md"
+	git mv "$file" "$(echo "$name_only" | tr '[:lower:]' '[:upper:]').md"
 done
